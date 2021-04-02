9da81a8a2d275efb692b4f8d3eef9cb8ff339630
Milo Cage <48653177+Thuziel@users.noreply.github.com>
2021-04-02 14:13:32 -0500

    Update macro.py

--- macro.py
+++ macro.py
@@ -1,23 +1,27 @@
 from pynput.keyboard import Key, Listener, KeyCode, Controller
 import time
 
-
-
 run = 'q'
-timeout = 0.1
-thingSwitch = '2'
+close = '0'
+inventorySlot = '2'
 editKey = 'k'
 resetEditKey ='j'
 
+timeout = 0.1
 
-
+quit_key = KeyCode(char=close)
 reset_key = KeyCode(char=run)
 keyboard = Controller()
 
+print("Marco successfully setup.\n")
+print("Controls:\n    "+run+" to run\n    "+close+" to close\n")
+print("Current Fortnite keybinds:\n    "+inventorySlot+" what inventory slot it switches to\n    "+editKey+" to edit\n    "+resetEditKey+" to reset edits\n")
+print("If you would like to change any of these you can do so on lines 4-8.")
+
 def on_press(key):
     if key == reset_key:
-        keyboard.press(thingSwitch)
-        keyboard.release(thingSwitch)
+        keyboard.press(inventorySlot)
+        keyboard.release(inventorySlot)
 
         time.sleep(timeout) 
         
@@ -33,6 +37,8 @@ def on_press(key):
 
         keyboard.press(editKey)
         keyboard.release(editKey)
+    if key == quit_key:
+        quit()
 
 with Listener(on_press=on_press) as listener:
     listener.join()
